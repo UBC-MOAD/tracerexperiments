fd3db690f8ab013f92789a8170eee584859ae09c
Karina Ramos Musalem <kramosmu@eos.ubc.ca>
2017-02-25 11:35:23 -0800

    Updated 3DVISC and CNTDIFF runs (changing f) to use 2 tracers and free slip bbc+drag

--- 3DVISC/code/PTRACERS_SIZE.h
+++ 3DVISC/code/PTRACERS_SIZE.h
@@ -16,7 +16,7 @@ C  and is set here (default 1)
 C
 C     Number of tracers
       INTEGER PTRACERS_num
-      PARAMETER(PTRACERS_num = 1 )
+      PARAMETER(PTRACERS_num = 3 )
 
 CEOP
 #endif /* ALLOW_PTRACERS */
