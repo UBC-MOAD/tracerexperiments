51bdaa0ae1f683ed855d971a4f99b3ca5c27b3d3
Karina Ramos Musalem <Karina Ramos Musalem@localhost>
2017-11-04 22:19:18 -0400

    runs LOW, LOWER, LOWEST fixed for graham

---
+++ LOWEST_BF/run04/glueDiagnosticsjob.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=glueDiagnostics_LOWESTBFrun04  
+#SBATCH --ntasks=1 
+#SBATCH --mem=100Gb
+#SBATCH --time=02:00:00
+#SBATCH --mail-user=kramosmu@eos.ubc.ca
+#SBATCH --mail-type=ALL
+#SBATCH --account=def-allen
+
+
+module load netcdf-mpi/4.4.1.1
+module load netcdf-fortran-mpi/4.4.4
+module load python/2.7.13 
+
+
+cd /scratch/kramosmu/MITgcm/tracerexperiments/LOWEST_BF_run04
+
+echo "Current working directory is `pwd`"
+
+echo "Diagnostics combining"
+bash $PROJECT/kramosmu/MITgcm/utils/scripts/glueDiagnostics.sh
+bash $PROJECT/kramosmu/MITgcm/utils/scripts/deflateDiagnostics.sh
+echo "done combining and deflating diagnostics"
