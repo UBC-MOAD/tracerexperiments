a8a67797c0e812dbbd619bd68daceec476911575
Karina Ramos Musalem <kramosmu@eoas.ubc.ca>
2017-11-04 22:19:18 -0400

    runs LOW, LOWER, LOWEST fixed for graham

---
+++ LOWEST_BF/run07/mitgcm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=LOWEST_BF_run07
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=100GB
+#SBATCH --time=06:00:00
+#SBATCH --mail-user=kramosmu@eos.ubc.ca
+#SBATCH --mail-type=ALL
+#SBATCH --account=def-allen
+
+
+module load netcdf-mpi/4.4.1.1
+module load netcdf-fortran-mpi/4.4.4
+module load python
+
+
+echo "Current working directory is `pwd`"
+
+echo "Starting run at: `date`"
+mpirun -np 112 ./mitgcmuv 
+echo "Job finished at: `date`"
+
+
