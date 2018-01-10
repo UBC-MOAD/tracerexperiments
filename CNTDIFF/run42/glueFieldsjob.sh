3277f2ebea4060d9f84b5baf20f1526254142cfd
Karina Ramos Musalem <kramosmu@eoas.ubc.ca>
2018-01-10 12:34:27 -0500

    added data files for a bunch of runs

---
+++ CNTDIFF/run42/glueFieldsjob.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=glueFieldsCNT42
+#SBATCH --ntasks=1
+#SBATCH --mem=100Gb
+#SBATCH --time=00:30:00
+#SBATCH --mail-user=kramosmu@eos.ubc.ca
+#SBATCH --mail-type=ALL
+#SBATCH --account=def-allen
+
+
+module load netcdf-mpi/4.4.1.1
+module load netcdf-fortran-mpi/4.4.4
+module load python/2.7.13                     
+
+
+cd /scratch/kramosmu/MITgcm/tracerexperiments/CNTDIFF_run42
+
+echo "Current working directory is `pwd`"
+
+echo "Fields combining"
+bash $PROJECT/kramosmu/MITgcm/utils/scripts/glueFields.sh
+bash $PROJECT/kramosmu/MITgcm/utils/scripts/deflateFields.sh
+echo "done combining and deflating fields"
+
