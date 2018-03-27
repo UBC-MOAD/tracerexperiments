a3f068a2a16bee2e7111df9a048563af50c3bad6
Karina Ramos Musalem <kramosmu@eoas.ubc.ca>
2018-03-27 16:55:58 -0700

    Added runs from experiemnts with the Step, realistic kv and DIC and Alkalinity

---
+++ CNTDIFF_STEP/run42_adv81/mitgcm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=CNTDIFF_STEP_run42_adv81 
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem=100GB
+#SBATCH --time=06:00:00
+#SBATCH --mail-user=kramosmu@eos.ubc.ca
+#SBATCH --mail-type=ALL
+#SBATCH --account=def-allen
+
+module load netcdf-mpi/4.4.1.1
+module load netcdf-fortran-mpi/4.4.4
+module load python
+
+echo "Current working directory is `pwd`"
+
+echo "Starting run at: `date`"
+mpirun -np 112 ./mitgcmuv
+echo "Job finished at: `date`" 
---
+++ LOWEST_BF_STEP/run02/glueDiagnostics.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=glueDiagnosticsLOWEST_BF_STEP_run02 
+#SBATCH --ntasks=1
+#SBATCH --mem=100Gb
+#SBATCH --time=02:00:00
+#SBATCH --mail-user=kramosmu@eos.ubc.ca
+#SBATCH --mail-type=ALL
+#SBATCH --account=def-allen
+
+module load netcdf-mpi/4.4.1.1
+module load netcdf-fortran-mpi/4.4.4
+module load python/2.7.13
+cd /scratch/kramosmu/MITgcm/tracerexperiments/LOWEST_BF_STEP_run02 
+echo "Current working directory is `pwd`"
+
+echo "Fields combining"
+bash $PROJECT/kramosmu/MITgcm/utils/scripts/glueDiagnostics.sh
+bash $PROJECT/kramosmu/MITgcm/utils/scripts/deflateDiagnostics.sh
+echo "done combining and deflating fields"
