7a8bb941ccde449621a4b17a7b9fbdc15a0c738b
Karina Ramos Musalem <kramosmu@eoas.ubc.ca>
2018-06-05 18:59:43 -0400

    Added runs 25-28 in 3D realistic exp

---
+++ 3DVISC_REALISTIC/run25/glueDiagnosticsjob.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --job-name=glueDiagnostics_3DVISC25  
+#SBATCH --ntasks=1 
+#SBATCH --mem=100Gb
+#SBATCH --time=01:00:00
+#SBATCH --mail-user=kramosmu@eos.ubc.ca
+#SBATCH --mail-type=ALL
+#SBATCH --account=def-allen
+
+
+module load netcdf-mpi/4.4.1.1
+module load netcdf-fortran-mpi/4.4.4
+module load python/2.7.13 
+
+
+cd /scratch/kramosmu/MITgcm/tracerexperiments/3DVISC_REALISTIC_run25
+
+echo "Current working directory is `pwd`"
+
+echo "Diagnostics combining"
+bash $PROJECT/kramosmu/MITgcm/utils/scripts/glueDiagnostics.sh
+bash $PROJECT/kramosmu/MITgcm/utils/scripts/deflateDiagnostics.sh
+echo "done combining and deflating diagnostics"
